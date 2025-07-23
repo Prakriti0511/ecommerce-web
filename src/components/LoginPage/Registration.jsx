a8de069a6b42601bc571e82e1c48dc6043498b4b
Prakriti0511 <prakriti0511@gmail.com>
2025-07-24 00:59:37 +0530

    login ui

--- src/components/LoginPage/Registration.jsx
+++ src/components/LoginPage/Registration.jsx
@@ -13,13 +13,21 @@ function Registration() {
             <span className='text-[25px] font-semibold pb-6 font-gill'>Registration Page</span>
             <span className = 'text-[16px] font-semibold'>Welcome to Lush!</span>
         </div>
-        <div className = 'max-w-[600px] w-[90%] h-[500px] bg-[#FFF2E0] border border-white backdrop-blur-2xl rounded-lg shadow-lg flex items-center justify-center mx-auto'>
+        <div className = 'max-w-[600px] w-[90%] h-[500px] bg-[#FFF2E0] rounded-[10px] border border-white backdrop-blur-2xl shadow-lg flex items-center justify-center mx-auto'>
             <form action="" className='w-[90%] h-[90%] flex flex-col items-center justify-start gap-[20px]'>
-                <div className = 'w-[90%] h-[50px] bg-[#898AC4] text-[black] border-2 border-[white] rounded-xl shadow-lg flex items-center justify-center gap-[10px] py-[20px] cursor-pointer'>
-                    <img src = "google.png" alt="/Image/" className='w-[20px]'/> Signup with Google
+                <div className = 'w-[90%] h-[50px] bg-[#898AC4] text-[black] border-2 border-[#C0C9EE] rounded-[10px] shadow-lg flex items-center justify-center gap-[10px] py-[20px] cursor-pointer'>
+                    <img src = "google.png" alt="/Image/" className='w-[20px] rounded-[10px]'/> Signup with Google
                 </div>
                 <div className='w-[100%] h-[20px] flex items-center justify-center gap-[10px]'>
                     <div className='w-[40%] h-[1px] bg-[#898AC4]'></div> OR <div className='w-[40%] h-[1px] bg-[#898AC4]'></div>
+                </div>
+                <div className = 'w-[90%] h-[400px] flex flex-col items-center justify-center gap-[15px]'>
+                    <input type = "text" className = 'w-[80%] h-[50px] border-[2px] border-[#C0C9EE] backdrop:blur-sm rounded-[10px] shadow-lg bg-transparent placeholder-[#] px-[20px] font-semibold' placeholder = 'UserName' required/>
+                    <input type = "text" className = 'w-[80%] h-[50px] border-[2px] border-[#C0C9EE] backdrop:blur-sm rounded-[10px] shadow-lg bg-transparent placeholder-[#] px-[20px] font-semibold' placeholder = 'Email' required/>
+                    <input type = "password" className = 'w-[80%] h-[50px] border-[2px] border-[#C0C9EE] backdrop:blur-sm rounded-[10px] shadow-lg bg-transparent placeholder-[#] px-[20px] font-semibold' placeholder = 'Password' required/>
+                    <button className='w-[100%] h-[50px] bg-[#898AC4] rounded-[10px] flex items-center justify-center mt-[20px] text-[17px] font-semibold'> Create Account </button>
+                    <p className='flex gap-[10px]'>You have any account? <span className='text-[#] text-[17px] font-semibold cursor-pointer'>Login</span></p>
+                    
                 </div>
             </form>
         </div>
