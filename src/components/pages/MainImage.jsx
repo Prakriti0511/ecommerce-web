42b1be97302ef48d2230ccc48f39608e03de2735
Prakriti0511 <prakriti0511@gmail.com>
2026-03-13 12:00:26 +0530

    homepage on existing UI Improvements

--- src/components/pages/MainImage.jsx
+++ src/components/pages/MainImage.jsx
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom";
 
 function MainImage() {
   return (
-    <div></div>
+    <div className="relative w-full h-[400px] bg-gradient-to-r from-blue-400 to-purple-500 flex items-center justify-center text-white text-4xl font-bold">
+      Welcome to Our Store
+    </div>
   );
 }
 
